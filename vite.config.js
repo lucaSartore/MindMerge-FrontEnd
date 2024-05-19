cc2c040f9c5c8ef8ed449947ac4c319b50f39877
Luca Sartore <lucasar.com8@gmail.com>
2024-05-19 20:53:24 +0200

    deploy test

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production'
-          ? '/MindMerge/' // prod
-          : '/', // dev
+  base: '/MindMerge/',
 })
