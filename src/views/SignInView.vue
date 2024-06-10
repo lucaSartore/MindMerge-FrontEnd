ae73441317d04f7fda8c0352fd2d3cedcc3dfd03
gioele-bernardini <gioele.g@outlook.com>
2024-06-10 15:52:13 +0200

    botton updated.

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,22 +1,21 @@
 <script setup>
+import { ref } from 'vue';
+
+const errorMessage = ref('');
+
+const handleGoogleSignIn = () => {
+};
 </script>
 
 <template>
   <div class="sign-in-view">
     <h2>Sign In</h2>
-    <form @submit.prevent="handleSignIn">
-      <div>
-        <label for="username">Username</label>
-        <input type="text" id="username" v-model="username" />
-      </div>
-      <div>
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model="password" />
-      </div>
-      <button type="submit">Sign In</button>
-    </form>
+    <button @click="handleGoogleSignIn">Sign In with Google</button>
     <p v-if="errorMessage">{{ errorMessage }}</p>
   </div>
 </template>
 
-<!-- TODO: GIOELE -->
+<style scoped>
+.sign-in-view {
+}
+</style>
