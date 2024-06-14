8ed88945111804350104ea38350b1866f288689b
gioele-bernardini <gioele.g@outlook.com>
2024-06-14 16:22:14 +0200

    image fixed

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,15 +1,6 @@
-<script setup>
-import { ref } from 'vue';
-
-const errorMessage = ref('');
-
-const handleGoogleSignIn = () => {
-  window.location.href = '/auth/google';
-};
-</script>
-
 <template>
   <div class="sign-in-view">
+    <img id="logo" src="#">
     <h2>Sign In</h2>
     <button class="google-sign-in-button" @click="handleGoogleSignIn">
       <img src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA" alt="Google icon" class="google-icon" />
@@ -29,10 +20,16 @@ const handleGoogleSignIn = () => {
   background-color: #f5f5f5;
 }
 
+.sign-in-view #logo {
+  padding: 0 0;
+  margin: 20px 0;
+  width: 30vh;
+}
+
 .google-sign-in-button {
-  background-color: white;
-  color: #757575;
-  border: 1px solid #dcdcdc;
+  background-color: white; /* Background bianco */
+  color: #757575; /* Colore del testo grigio */
+  border: 1px solid #dcdcdc; /* Bordo grigio chiaro */
   padding: 10px 24px;
   font-size: 16px;
   font-weight: bold;
@@ -46,12 +43,12 @@ const handleGoogleSignIn = () => {
 }
 
 .google-sign-in-button:hover {
-  background-color: #f1f1f1;
+  background-color: #f1f1f1; /* Colore di sfondo al passaggio del mouse */
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 }
 
 .google-sign-in-button:active {
-  background-color: #e1e1e1;
+  background-color: #e1e1e1; /* Colore di sfondo quando premuto */
 }
 
 .google-icon {
@@ -64,6 +61,3 @@ const handleGoogleSignIn = () => {
   white-space: nowrap;
 }
 </style>
-
-<!-- TODO: add sign_in_image.jpg above the botton
-TODO: implement the button functionality -->
