a932f77cbedc3b55497b2a1e910996fe56a48972
gioele-bernardini <gioele.g@outlook.com>
2024-06-22 10:01:59 +0200

    sign up updated

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -6,26 +6,76 @@
     const redirectUrl = await getGoogleRedirectUrl();
     window.location.href = redirectUrl;
   }
-
-
 </script>
 
- <template>
-
-  <main>
-    <button @click="logInWithGoogle" class="google-signup-button">
-      Sign up with Google
+<template>
+  <div class="sign-up-view">
+    <p class="signup-description">Join us today and enjoy exclusive features!</p>
+    <img id="logo" src="../../public/sign_in.png">
+    <h2>Sign Up</h2>
+    <button class="google-sign-in-button" @click="logInWithGoogle">
+      <img src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA" alt="Google icon" class="google-icon" />
+      <span class="button-text">Sign Up with Google</span>
     </button>
-  </main>
+    <p v-if="errorMessage">{{ errorMessage }}</p>
+  </div>
 </template>
 
+<style scoped>
+.sign-up-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+}
 
+.signup-description {
+  font-size: 20px;
+  color: #333;
+  margin-bottom: 20px;
+  text-align: center;
+}
 
+.sign-up-view #logo {
+  padding: 0 0;
+  margin: 20px 0;
+  width: 30vh;
+}
 
-<!-- <template>
-  <main>
-    <h1>Sign Up</h1>
-    This is where you can sign up to the MindMerge platform.
+.google-sign-in-button {
+  background-color: white;
+  color: #757575;
+  border: 1px solid #dcdcdc;
+  padding: 10px 24px;
+  font-size: 16px;
+  font-weight: bold;
+  border-radius: 4px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.3s ease, box-shadow 0.3s ease;
+}
 
-  </main>
-</template> -->
+.google-sign-in-button:hover {
+  background-color: #f1f1f1;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+.google-sign-in-button:active {
+  background-color: #e1e1e1;
+}
+
+.google-icon {
+  margin-right: 8px;
+  width: 20px;
+  height: 20px;
+}
+
+.button-text {
+  white-space: nowrap;
+}
+</style>
