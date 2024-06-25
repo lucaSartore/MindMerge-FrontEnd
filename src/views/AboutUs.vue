56263996073b54e948b1c15028c2d74df2081e66
gioele-bernardini <gioele.g@outlook.com>
2024-06-25 12:30:25 +0200

    abous us page updated

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -5,5 +5,7 @@
   <main>
     <h1>About Us</h1>
     Mind merge is an open source task management software with novel AI features integrated.
+    Mindmerge is a web application focused on optimizing workflow. The app allows users to create work schedules and tasks to assist businesses in dividing workloads. The flagship feature is the system - integrated with modern language models - that provides ***fully automated reports*** on personnel productivity, based on notes and comments provided by employees themselves in an easy and flexible manner.
+    check out more infos at: https://github.com/lucaSartore/MindMerge
   </main>
 </template>
